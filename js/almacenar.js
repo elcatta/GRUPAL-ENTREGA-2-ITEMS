3e01a2ead3ee382662741d619759652128cb586c
ElCatta <cattalurdai@gmail.com>
2022-08-18 11:06:15 -0300

    Adición de items desarrollada
    lista es guardada en localstorage. Falta desarrollar el boton de limpiar y debuggear como se añade el texto a CONTENEDOR. El resto, bien

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,7 +1,15 @@
 const CONTAINER = document.getElementById("contenedor")
-
+if (localStorage.getItem('lista') != null){
+CONTAINER.innerHTML = localStorage.getItem('lista')
+}
 
 function addText(){
-let appendedTxt = document.getElementById("item").value
-CONTAINER.innerHTML += '<li>' + appendedTxt + '</li>'
+    let appendedTxt = document.getElementById("item").value
+    localStorage.setItem('lista', localStorage.getItem('lista') + '<li>' + appendedTxt + '</li>')
+    document.getElementById("item").value = ""
+    CONTAINER.innerHTML = localStorage.getItem('lista')
 }
+
+
+
+
